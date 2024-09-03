863d7c0269b5a15b3e8d7190de2d4387092fae46
MomDra <yem9087@gmail.com>
2024-09-03 20:41:13 +0900

    Revert "블럭 게임 프로젝트 생성"
    This reverts commit 9d1e1a71b4de47fefb5dc2f9d57e87545b19c266.

--- WinAPI_BlockGame/WinAPI_BlockGame/Matrix3x3.cpp
+++
@@ -1,4 +0,0 @@
-#include "Vector3.h"
-#include "Matrix3x3.h"
-
-constexpr Matrix3x3 Matrix3x3::Identity{ Vector3{1.0f, 0.0f, 0.0f}, Vector3{0.0f, 1.0f, 0.0f}, Vector3{0.0f, 0.0f, 1.0f} };
--- WinAPI_BlockGame/WinAPI_BlockGame/Vector2.cpp
+++
@@ -1,4 +0,0 @@
-#include "Vector2.h"
-
-constexpr Vector2 Vector2::UnitX{ 1.0f, 0.0f };
-constexpr Vector2 Vector2::UnitY{ 0.0f, 1.0f };
