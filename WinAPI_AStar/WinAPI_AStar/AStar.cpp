7b1623d255955d36db6a789f2ffb06a144d93932
MomDra <yem9087@gmail.com>
2024-09-28 04:36:04 +0900

    A* 알고리즘 완성

--- WinAPI_AStar/WinAPI_AStar/AStar.cpp
+++ WinAPI_AStar/WinAPI_AStar/AStar.cpp
@@ -3,6 +3,7 @@
 #include <unordered_set>
 #include <queue>
 #include <array>
+#include <stack>
 
 struct Node
 {
@@ -25,90 +26,182 @@ namespace std
 	template<>
 	struct hash<Node>
 	{
-		std::size_t operator()(const Node& node) const
+		std::size_t operator()(const Node& node) const noexcept
 		{
 			std::hash<int> hash_func;
 
 			return hash_func(node.x) ^ (hash_func(node.y) << 1);
 		}
 	};
+
+	template<>
+	struct hash<std::pair<int, int>>
+	{
+		std::size_t operator()(const std::pair<int, int>& pair) const noexcept
+		{
+			std::hash<int> hash_func;
+
+			return hash_func(pair.first) ^ (hash_func(pair.second) << 1);
+		}
+	};
 }
 
-std::vector<std::pair<int, int>> AStar::FindPath() const noexcept
+//std::vector<std::pair<int, int>> AStar::FindPath() const noexcept
+//{
+//	// F(x) = G(x) + H(x)
+//	// 아직 평가를 해야할 노드
+//	std::priority_queue<Node> openSet;
+//
+//	// 평가를 마친 노드
+//	std::unordered_set<int> closedSet;
+//
+//	std::unordered_map<int, std::unordered_map<int, int>> gCost;
+//	std::unordered_map<int, std::unordered_map<int, std::pair<int, int>>> cameFrom;
+//
+//	// 처음에는 시작 노드로 시작한다!
+//	openSet.emplace(startX, startY, 0.0, Heuristic(startX, startY, goalX, goalY));
+//	gCost[startX][startY] = 0;
+//
+//	std::array<std::pair<int, int>, 4> directions{ { {0, 1}, {1, 0}, {0, -1}, {-1, 0} } };
+//	std::pair<int, int> minusPair{ std::make_pair(-1, -1) };
+//
+//	while (!openSet.empty())
+//	{
+//		Node current{ openSet.top() };
+//		openSet.pop();
+//
+//		// 목표에 도착하면 경로 구성
+//		if (current.x == goalX && current.y == goalY)
+//		{
+//			std::vector<std::pair<int, int>> path;
+//
+//			int x{ current.x };
+//			int y{ current.y };
+//
+//			while (cameFrom[x][y] != minusPair)
+//			{
+//				path.emplace_back(x, y);
+//				const auto& [prevX, prevY] {cameFrom[x][y]};
+//
+//				x = prevX;
+//				y = prevY;
+//			}
+//
+//			path.emplace_back(startX, startY);
+//			std::reverse(path.begin(), path.end());
+//
+//			return path;
+//		}
+//
+//		// 현재 노드를 closed set에 추가
+//		closedSet.emplace(current.x * grid[0].size() + current.y);
+//
+//		// 인접한 노드 검사
+//		for (const auto& [dx, dy] : directions)
+//		{
+//			int neighborX{ current.x + dx };
+//			int neighborY{ current.y + dy };
+//
+//			// 맵 범위 체크 및 장애물 검사
+//			if (neighborX >= 0 && neighborY >= 0 && neighborX < grid.size() && neighborY < grid[0].size() && grid[neighborX][neighborY] == 0)
+//			{
+//				int neighborKey{ neighborX * static_cast<int>(grid[0].size()) + neighborY };
+//
+//				// 이미 ClosedSet에 있는 노드는 무시
+//				
+//				if (closedSet.find(neighborKey) != closedSet.end())
+//					continue;
+//
+//				int tentativeScore{ gCost[current.x][current.y] + 1 };
+//
+//				if (gCost.find(neighborX) == gCost.end() || gCost[neighborX].find(neighborY) == gCost[neighborX].end() || tentativeScore < gCost[neighborX][neighborY])
+//				{
+//					cameFrom[neighborX][neighborY] = { current.x, current.y };
+//					gCost[neighborX][neighborY] = tentativeScore;
+//
+//					int h{ Heuristic(neighborX, neighborY, goalX, goalY) };
+//					openSet.emplace(neighborX, neighborY, tentativeScore, h);
+//				}
+//			}
+//		}
+//	}
+//
+//	return std::vector<std::pair<int, int>>{};
+//}
+
+std::vector<std::pair<int, int>> AStar::FindPath2() const noexcept
 {
 	// F(x) = G(x) + H(x)
 	// 아직 평가를 해야할 노드
 	std::priority_queue<Node> openSet;
 
+	std::priority_queue<Node> openSet2;
+
 	// 평가를 마친 노드
-	std::unordered_set<int> closedSet;
-
-	std::unordered_map<int, std::unordered_map<int, int>> gCost;
-	std::unordered_map<int, std::unordered_map<int, std::pair<int, int>>> cameFrom;
-
-	// 처음에는 시작 노드로 시작한다!
-	openSet.emplace(startX, startY, 0.0, Heuristic(startX, startY, goalX, goalY));
-	gCost[startX][startY] = 0;
-
+	std::unordered_set<std::pair<int, int>> closeSet;
+	std::unordered_map<std::pair<int, int>, int> gCost;
+	std::unordered_map<std::pair<int, int>, std::pair<int, int>> cameFrom;
 	std::array<std::pair<int, int>, 4> directions{ { {0, 1}, {1, 0}, {0, -1}, {-1, 0} } };
-	std::pair<int, int> minusPair{ std::make_pair(-1, -1) };
+
+	openSet.emplace(startX, startY, 0.0, Heuristic(startX, startY, goalX, goalY));
+	// 처음에는 시작 노드로 시작한다!
+
+	std::pair<int, int> tmp;
 
 	while (!openSet.empty())
 	{
 		Node current{ openSet.top() };
 		openSet.pop();
 
-		// 목표에 도착하면 경로 구성
+		closeSet.emplace(current.x, current.y);
+
 		if (current.x == goalX && current.y == goalY)
 		{
 			std::vector<std::pair<int, int>> path;
+			std::stack<std::pair<int, int>> st;
 
-			int x{ current.x };
-			int y{ current.y };
+			tmp = { current.x, current.y };
 
-			while (cameFrom[x][y] != minusPair)
+			while (cameFrom.find(tmp) != cameFrom.end())
 			{
-				path.emplace_back(x, y);
-				const auto& [prevX, prevY] {cameFrom[x][y]};
-
-				x = prevX;
-				y = prevY;
+				st.emplace(tmp);
+				tmp = cameFrom[tmp];
 			}
 
-			path.emplace_back(startX, startY);
-			std::reverse(path.begin(), path.end());
+			st.emplace(startX, startY);
+
+			path.reserve(st.size());
+
+			while (!st.empty())
+			{
+				path.emplace_back(st.top());
+				st.pop();
+			}
 
 			return path;
 		}
 
-		// 현재 노드를 closed set에 추가
-		closedSet.emplace(current.x * grid[0].size() + current.y);
-
-		// 인접한 노드 검사
 		for (const auto& [dx, dy] : directions)
 		{
-			int neighborX{ current.x + dx };
-			int neighborY{ current.y + dy };
+			int nx{ current.x + dx };
+			int ny{ current.y + dy };
 
-			// 맵 범위 체크 및 장애물 검사
-			if (neighborX >= 0 && neighborY >= 0 && neighborX < grid.size() && neighborY < grid[0].size() && grid[neighborX][neighborY] == 0)
+			if (nx >= 0 && ny >= 0 && nx < grid.size() && ny < grid[0].size())
 			{
-				int neighborKey{ neighborX * grid[0].size() + neighborY };
+				tmp = { nx, ny };
 
-				// 이미 ClosedSet에 있는 노드는 무시
-				
-				if (closedSet.find(neighborKey) != closedSet.end())
-					continue;
-
-				int tentativeScore{ gCost[current.x][current.y] + 1 };
-
-				if (gCost.find(neighborX) == gCost.end() || gCost[neighborX].find(neighborY) == gCost[neighborX].end() || tentativeScore < gCost[neighborX][neighborY])
+				if (closeSet.find(tmp) == closeSet.end())
 				{
-					cameFrom[neighborX][neighborY] = { current.x, current.y };
-					gCost[neighborX][neighborY] = tentativeScore;
+					int newGCost{ current.g + 10 };
 
-					int h{ Heuristic(neighborX, neighborY, goalX, goalY) };
-					openSet.emplace(neighborX, neighborY, tentativeScore, h);
+					if (gCost.find(tmp) == gCost.end() || gCost[tmp] < newGCost)
+					{
+						gCost[tmp] = newGCost;
+						cameFrom[tmp] = std::make_pair(current.x, current.y);
+
+						int h{ Heuristic(nx, ny, goalX, goalY) };
+						openSet.emplace(nx, ny, newGCost, h);
+					}
 				}
 			}
 		}
@@ -117,38 +210,24 @@ std::vector<std::pair<int, int>> AStar::FindPath() const noexcept
 	return std::vector<std::pair<int, int>>{};
 }
 
-std::vector<std::pair<int, int>> AStar::FindPath2() const noexcept
-{
-	std::priority_queue<Node> openSet;
-	std::array<std::pair<int, int>, 4> directions{ { {0, 1}, {1, 0}, {0, -1}, {-1, 0} } };
-
-	while (!openSet.empty())
-	{
-		Node current{ openSet.top() };
-		openSet.pop();
-
-		if (current.x == goalX && current.y == goalY)
-			break;
-
-		for (const auto& [dx, dy] : directions)
-		{
-			int nx{ current.x + dx };
-			int ny{ current.y + dy };
-
-			if (nx >= 0 && ny >= 0 && nx < grid.size() && ny < grid.size())
-			{
-				int nKey{ nx * grid[0].size() + ny };
-
-
-			}
-		}
-	}
-
-	return std::vector<std::pair<int, int>>();
-}
-
 // Manhatan Distance
 int AStar::Heuristic(int x1, int y1, int x2, int y2) const noexcept
 {
-	return std::abs(x1 - x2) + std::abs(y1 - y2);
+	int dx{ std::abs(x1 - x2) };
+	int dy{ std::abs(y1 - y2) };
+
+	int min{ std::min(dx, dy) };
+	int max{ std::max(dx, dy) };
+
+	return 14 * min + 10 * (max - min);
+}
+
+std::vector<std::pair<int, int>> AStar::RetracePath()
+{
+	std::vector<std::pair<int, int>> track;
+
+
+
+
+	return track;
 }
--- WinAPI_AStar/WinAPI_AStar/AStar.h
+++ WinAPI_AStar/WinAPI_AStar/AStar.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <array>
 
 class AStar
 {
@@ -7,6 +8,11 @@ class AStar
 	{
 		int x, y;
 		int g, h;
+
+		explicit Node(int x, int y, int g, int h) : x{ x }, y{ y }, g{ g }, h{ h }
+		{
+
+		}
 
 		double F() const noexcept
 		{
@@ -17,17 +23,22 @@ class AStar
 		{
 			return F() > other.F();
 		}
+
+	private:
+		static constexpr std::array<std::pair<int, int>, 8> directions{};
 	};
 
 public:
-	explicit AStar(const std::vector<std::vector<int>>& grid, int startX, int startY, int goalX, int goalY)
+	explicit AStar(const std::vector<std::vector<Node>>& grid, int startX, int startY, int goalX, int goalY)
 		: grid{ grid }, startX{ startX }, startY{ startY }, goalX{ goalX }, goalY{ goalY } {}
 
-	std::vector<std::pair<int, int>> FindPath() const noexcept;
+	//std::vector<std::pair<int, int>> FindPath() const noexcept;
 	std::vector<std::pair<int, int>> FindPath2() const noexcept;
 	int Heuristic(int x1, int y1, int x2, int y2) const noexcept;
 
 private:
-	std::vector<std::vector<int>> grid;
+	std::vector<std::vector<Node>> grid;
 	int startX, startY, goalX, goalY;
+
+	std::vector<std::pair<int, int>> RetracePath();
 };
